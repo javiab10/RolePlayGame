22cc34f8a0cba7e468fbc2545227c5cc8e161c64
javiab10 <javierbasoko@gmail.com>
2024-11-13 20:36:45 +0100

    Primera arquitectura y páginas provisionales
    Se ha creado el inicio de la arquitectura del proyecto y las dos páginas principales con un diseño provisional

---
+++ app/views/public/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Artean</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark h-50">
+            <div class="container w-25 p-0"><a class="navbar-brand" href="#" class="w-25"><img src="../../../assets/img/heroesLogo.png" alt="" class="img-fluid"></a></div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a  class="nav-link text-white" href="insert.php">Crear una criatura</a>
+                    </li>
+                </ul>
+            </div>  
+        </nav>
+        
+        <main>
+            <div class="container">
+                <div class="row mt-5 mb-5">
+                    <div class="col-lg-6">
+                        <img src="../../../assets/img/heroesPortada.jpg" class="img-fluid">
+                    </div>
+                    <div class="col-lg-6">
+                        <h4>Comunidad de usuarios de Heroes</h4>
+                        <p>La aventura comienza aquí, en tu navegador</p>
+                        <button type="button" class="btn btn-primary">Juega ahora!</button>
+                    </div>
+                </div>
+                
+                <hr class="mb-5">
+                
+                <div class="row mb-5">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <h3 class="card-title">Nombre de la Criatura</h3>
+                            <div class="row card-body">
+                                <div class="col-6"><img src="../../../assets/img/creatureSample.jpg" class="img-fluid"></div>
+                                <div class="col-6"><p class="card-text">Descripicón criatura de ejemplo</p>
+                            </div>
+                            <div class="card-footer bg-white d-flex justify-content-around mt-2">
+                                <button type="button" class="btn btn-secondary">Más info</button>
+                                <button type="button" class="btn btn-success">Modificar</button>
+                                <button type="button" class="btn btn-danger">Eliminar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+        
+        <!-- Bootstrap Script -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ app/views/public/insert.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Artean</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark h-50">
+            <div class="container w-25 p-0"><a class="navbar-brand" href="#" class="w-25"><img src="../../../assets/img/heroesLogo.png" alt="" class="img-fluid"></a></div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a  class="nav-link text-white" href="index.php">Inicio</a>
+                    </li>
+                </ul>
+            </div>  
+        </nav>
+        
+        <div class="container-fluid" id="formInsert">
+    <form action="" class="mt-5" id="">
+        <div class="container-fluid">
+            <div class="mb-3 row">
+                <label for="name" class="col-sm-2 col-form-label">
+                    Nombre
+                </label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="description" class="col-sm-2 col-form-label">
+                    Description
+                </label>
+                <div class="col-10">
+                    <input type="text" class="form-control" id="description" name="description" placeholder="Description">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="avatar" class="col-sm-2 col-form-label">
+                    Avatar
+                </label>
+                <div class="col-10">
+                    <input type="text" class="form-control" id="avatar" name="avatar" placeholder="Avatar">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="attackPower" class="col-sm-2 col-form-label">
+                    Attack Power
+                </label>
+                <div class="col-10">
+                    <input type="text" class="form-control" id="attackPower" name="attackPower" placeholder="Attack Power"></input>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="lifeLevel" class="col-sm-2 col-form-label">
+                    Attack Power
+                </label>
+                <div class="col-10">
+                    <input type="text" class="form-control" id="lifeLevel" name="lifeLevel" placeholder="lifeLevel">Life Level</input>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="weapon" class="col-sm-2 col-form-label">
+                    Attack Power
+                </label>
+                <div class="col-10">
+                    <input type="text" class="form-control" id="weapon" name="weapon" placeholder="Weapon">Weapon</input>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success" id="btnCrear">Crear</button>
+                </div>
+            </div>
+        </div>
+    </form>
+        
+    </body>
+</html>
